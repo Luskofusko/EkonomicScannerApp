8c1768be892fe86199f55f4ecb2bd6fbcd3aa7b1
aplourenco <applourenco@gmail.com>
2025-04-04 10:43:15 +0100

    - implement a delete icon to delete receipt on each item - add a dialog to confirm deletion

--- presentation/src/main/java/com/example/presentation/composable/ReceiptItem.kt
+++ presentation/src/main/java/com/example/presentation/composable/ReceiptItem.kt
@@ -8,11 +8,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -22,7 +32,7 @@ import com.example.domain.model.Receipt
 
 @Composable
 fun ReceiptItem(receipt: Receipt) {
-
+    var showDialog by remember { mutableStateOf(false) }
     val imagePainter = if (receipt.photoPath.toIntOrNull() != null) {
         // If photoPath is a resource ID
         painterResource(id = receipt.photoPath.toInt())
@@ -53,6 +63,38 @@ fun ReceiptItem(receipt: Receipt) {
                 Text(text = "Date: ${receipt.date}", style = MaterialTheme.typography.titleMedium)
                 Text(text = "Total: ${receipt.totalAmount} ${receipt.currency}", style = MaterialTheme.typography.bodyMedium)
             }
+            IconButton(
+                onClick = { showDialog = true },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete Receipt",
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
         }
+    }
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("Delete Receipt?") },
+            text = { Text("Are you sure you want to delete this receipt? This action cannot be undone.") },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        //onDelete()
+                        showDialog = false
+                    }
+                ) {
+                    Text("Delete", color = MaterialTheme.colorScheme.error)
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("Cancel")
+                }
+            }
+        )
     }
 }
